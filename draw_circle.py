46a9ade2ce6382c5d66e391fc3299a221815555a
Sidd <35938702+siddsp02@users.noreply.github.com>
2022-02-22 23:49:10 -0800

    Removed a level of nesting by using product.

--- draw_circle.py
+++ draw_circle.py
@@ -1,15 +1,16 @@
 # usr/bin/env python3
 
+from itertools import product
 from math import sqrt
 
 
 def draw_circle(r: int) -> None:
     d, rmin, rmax = r*2 + 1, -r, r+1
+    bounds = range(rmin, rmax)
     grid = [["."]*d for _ in range(d)]
-    for x in range(rmin, rmax):
-        for y in range(rmin, rmax):
-            if abs(sqrt(x*x + y*y) - r) < 0.5:
-                grid[x+r][y+r] = "@"
+    for x, y in product(bounds, repeat=2):
+        if abs(sqrt(x*x + y*y) - r) < 0.5:
+            grid[x+r][y+r] = "@"
     for row in grid:
         print(*row)
 
